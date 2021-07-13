b73d8a60f81b37d095f9cd51d7bd39fbdf20e44d
reynaldomorillo <reynaldomorillo.12@gmail.com>
2021-07-12 19:00:36 -0500

    06 uso de google fonts

--- App.js
+++ App.js
@@ -3,10 +3,28 @@ import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/theme";
-
 import StorePantalla from './src/caracteristicas/stores/pantallaStore/StorePantalla';
-
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import {
+  useFonts as useLato,
+  Lato_400Regular,
+} from '@expo-google-fonts/lato';
 export default function App() {
+
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <StorePantalla/>
--- src/caracteristicas/stores/componentStore/StoreInfo.js
+++ src/caracteristicas/stores/componentStore/StoreInfo.js
@@ -11,15 +11,17 @@ const StoreCardCover = styled(Card.Cover) `
     background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 const Title = styled.Text `
-    padding-left: ${(props) => props.theme.space[4]};
-    padding-bottom: ${(props) => props.theme.space[1]};
+    font-family: ${(props) => props.theme.fonts.heading}
+    font-size: ${(props) => props.theme.fontSizes.body}
     color: ${(props) => props.theme.colors.text.primary}
 `;
 const Address = styled(Text) `
-    padding-left: ${(props) => props.theme.space[4]};
-    padding-bottom: ${(props) => props.theme.space[4]};
+    font-family: ${(props) => props.theme.fonts.body}
+    font-size: ${(props) => props.theme.fontSizes.caption}
 `;
-
+const Info = styled(View) `
+    padding: ${(props) => props.theme.space[3]};
+`;
 export const StoreInfo = ({ store = {}}) => {
     const {
         name = "WAWA NEGRO CAFE",
@@ -36,8 +38,10 @@ export const StoreInfo = ({ store = {}}) => {
                 key={name}
                 source={{uri: photos[0]}}
             />
-            <Title>{name}</Title>
-            <Address>{address}</Address>
+            <Info>
+                <Title>{name}</Title>
+                <Address>{address}</Address>
+            </Info>
         </StoreCard>
     )
 }
